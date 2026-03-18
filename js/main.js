55247742f0fec4ba723079e0d1b711a87d38536f
Alex4747-J <npanh1903@gmail.com>
2026-03-18 18:36:17 -0400

    Add layout/docs, extra counters; remove Sass files
    Expanded index.html to include a header, navigation and detailed Overview/How-to/Examples sections, added additional counter containers, and switched the Google font to Audiowide. Removed the defer attribute from the module script. Updated js/main.js to instantiate four Counter instances (added counter3 and counter4). Deleted unused Sass partials (_fonts.scss, _index.scss, _mixins.scss, _variables.scss).

--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@ import { Counter } from './counter.js';
 
 const counter1 = new Counter('#counter-container1');
 const counter2 = new Counter('#counter-container2');
+const counter3 = new Counter('#counter-container3');
+const counter4 = new Counter('#counter-container4');
