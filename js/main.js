7de160f291f4d28b850857c18ea1806f37ec34eb
Alex4747-J <npanh1903@gmail.com>
2026-03-19 03:35:37 -0400

    Move counter to modules and add splittext
    Renamed js/counter.js -> js/modules/counter.js and updated main.js import to use the new modules path. Added a new js/modules/splittext.js exporting initSplitText which waits for fonts, sets paragraph opacity, and animates split lines with GSAP/SplitText. main.js now imports initSplitText and includes additional counter instances (counter3, counter4) from the stashed changes. Note: main.js contains unresolved merge conflict markers (<<<<<<< / ======= / >>>>>>>) that should be resolved.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-import { Counter } from './counter.js';
+import { Counter } from './modules/counter.js';
+import { initSplitText } from './modules/splittext.js';
 
 const counter1 = new Counter('#counter-container1');
+<<<<<<< Updated upstream
 const counter2 = new Counter('#counter-container2');
+=======
+const counter2 = new Counter('#counter-container2');
+const counter3 = new Counter('#counter-container3');
+const counter4 = new Counter('#counter-container4');
+
+initSplitText();
+>>>>>>> Stashed changes
